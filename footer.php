b3be1ba872622a8abcd7e35082a5ded1554add8c
Dmitry Pripa <i@dpripa.com>
2018-06-15 20:32:32 +0300

    ...

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container"></div>
-</footer>
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>window.jQuery||document.write('<scr'+'ipt src="static/jquery-3.2.1.min.js"><\/scr'+'ipt>')</script>
-<script src="static/scripts.min.js" defer></script>
-</body>
-</html>
