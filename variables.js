49b85bc90d0c3195dbad4efed3049fc13c7f2f1b
Dmitry Pripa <i@dpripa.com>
2018-07-01 21:55:27 +0300

    ...

--- variables.js
+++ variables.js
@@ -8,7 +8,7 @@ const _V = {
     SCR_SM_MIN: 768,
     SCR_MD_MIN: 992,
     SCR_LG_MIN: 1200,
-    SCR_XS_MAX: 767,
-    SCR_SM_MAX: 991,
-    SCR_MD_MAX: 1199
+    SCR_XS_MAX: 767, // SCR_SM_MIN - 1
+    SCR_SM_MAX: 991, // SCR_MD_MIN -1
+    SCR_MD_MAX: 1199 // SCR_LG_MIN - 1
 };
